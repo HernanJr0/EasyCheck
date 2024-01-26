41fdd72c8f7cc2b313909777d759f69cfe046a95
HernanJr0 <hernanjunior90@gmail.com>
2024-01-26 17:54:01 -0400

    fix: corrige task vazia 🐛

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,99 +1,154 @@
 import { Delete } from "@mui/icons-material";
-import { Divider, Paper, Checkbox, FormControlLabel, TextField, Button, Grid } from "@mui/material";
-import { useState, useEffect } from "react";
+import {
+  Alert,
+  Button,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  Grid,
+  Paper,
+  Snackbar,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 
 const AddTask = ({ updateTasks }: any) => {
-    const [task, setTask] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [task, setTask] = useState("");
 
-    const handleTaskChange = (event: any) => {
-        setTask(event.target.value);
-    };
+  const handleTaskChange = (event: any) => {
+    setTask(event.target.value);
+  };
 
-    const handleAddTaskClick = () => {
-        const newTask = `tsk${Math.random()}`;
-        localStorage.setItem(newTask, JSON.stringify(task));
-        updateTasks();
-        setTask('');
-    };
+  const handleSnackbarClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-    return (
-        <Grid container className="items-center">
-            <Grid item xs={9.5}>
-                <TextField
-                    size="small"
-                    fullWidth
-                    value={task}
-                    label="Digite a sua tarefa"
-                    onChange={handleTaskChange}
-                />
-            </Grid>
-            <Grid item xs={2.5}>
-                <Button onClick={handleAddTaskClick}>Adicionar</Button>
-            </Grid>
-        </Grid>
-    );
+    setSnackbarOpen(false);
+  };
+
+  const handleAddTaskClick = () => {
+    if (task != "") {
+      const newTask = `tsk${Math.random()}`;
+      localStorage.setItem(newTask, JSON.stringify(task));
+      updateTasks();
+      setTask("");
+    } else {
+      setSnackbarOpen(true);
+    }
+  };
+
+  return (
+    <Grid container className="items-center">
+      <Grid item xs={9.5}>
+        <TextField
+          size="small"
+          fullWidth
+          value={task}
+          label="Digite a sua tarefa"
+          onChange={handleTaskChange}
+        />
+      </Grid>
+      <Grid item xs={2.5}>
+        <Button onClick={handleAddTaskClick}>Adicionar</Button>
+      </Grid>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="info"
+          sx={{ width: "100%" }}
+        >
+          Insira uma tarefa
+        </Alert>
+      </Snackbar>
+    </Grid>
+  );
 };
 
 const Tasks = ({ updateTasks }: any) => {
-    const armazenamento = Object.entries(localStorage);
+  const armazenamento = Object.entries(localStorage);
 
-    const handleRemoveTask = (taskKey: string) => {
-        localStorage.removeItem(taskKey);
-        updateTasks();
-    };
+  const handleRemoveTask = (taskKey: string) => {
+    localStorage.removeItem(taskKey);
+    updateTasks();
+  };
 
-    if (localStorage.length != 0) {
-        return (
-            <div className="mt-4">
-                <Divider light />
-                {armazenamento.map(([taskKey, taskValue], index) => (
-                    <Grid container key={index} className="items-center justify-between">
-                        <Grid item>
-                            <FormControlLabel
-                                control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 32 } }} />}
-                                label={JSON.parse(taskValue)}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <Button color="error" size="small" onClick={() => handleRemoveTask(taskKey)}><Delete /></Button>
-                        </Grid>
-                    </Grid>
-                ))}
-            </div>
-        );
-    }
+  if (localStorage.length != 0) {
+    return (
+      <div className="mt-4">
+        <Divider light />
+        {armazenamento.map(([taskKey, taskValue], index) => (
+          <Grid container key={index} className="items-center justify-between">
+            <Grid item>
+              <FormControlLabel
+                control={
+                  <Checkbox sx={{ "& .MuiSvgIcon-root": { fontSize: 32 } }} />
+                }
+                label={JSON.parse(taskValue)}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                color="error"
+                size="small"
+                onClick={() => handleRemoveTask(taskKey)}
+              >
+                <Delete />
+              </Button>
+            </Grid>
+          </Grid>
+        ))}
+      </div>
+    );
+  }
 };
 
 const ActionButtons = ({ updateTasks }: any) => {
-    if (localStorage.length !== 0) {
-        return (
-            <Grid className="flex justify-end">
-                <Button color="error" size="small" onClick={() => { localStorage.clear(); updateTasks(); }}>Apagar todos</Button>
-            </Grid>
-        );
-    }
-    return null;
-}
+  if (localStorage.length !== 0) {
+    return (
+      <Grid className="flex justify-end">
+        <Button
+          color="error"
+          size="small"
+          onClick={() => {
+            localStorage.clear();
+            updateTasks();
+          }}
+        >
+          Apagar todos
+        </Button>
+      </Grid>
+    );
+  }
+  return null;
+};
 
 export default function Home() {
-    const [tasksUpdated, setTasksUpdated] = useState(false);
+  const [tasksUpdated, setTasksUpdated] = useState(false);
 
-    const updateTasks = () => {
-        setTasksUpdated(!tasksUpdated);
-    };
+  const updateTasks = () => {
+    setTasksUpdated(!tasksUpdated);
+  };
 
-    useEffect(() => {
-    }, [tasksUpdated]);
+  useEffect(() => {}, [tasksUpdated]);
 
-    return (
-        <>
-            <div className="flex justify-center items-center h-[100vh]">
-                <Paper className="w-[600px] p-4 text-center m-4">
-                    <AddTask updateTasks={updateTasks} />
-                    <Tasks updateTasks={updateTasks} />
-                    <ActionButtons updateTasks={updateTasks} />
-                </Paper>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh]">
+        <Paper className="w-[600px] p-4 text-center m-4">
+          <AddTask updateTasks={updateTasks} />
+          <Tasks updateTasks={updateTasks} />
+          <ActionButtons updateTasks={updateTasks} />
+        </Paper>
+      </div>
+    </>
+  );
 }
